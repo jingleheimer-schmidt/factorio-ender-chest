bca58ff8a11e6a0c4245e52d8c3d72ce516448b9
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-10 17:23:31 -0700

    still doesn't work

--- control.lua
+++ control.lua
@@ -1,13 +1,14 @@
---
--- -- runs when player joins is created
--- script.on_event(defines.events.on_player_created, function(player)
---   -- gets player id
---   local player_id = player.player_index
---   -- creates unique id for player ender chest
---   local ender_chest_id = "ender_chest_" .. player_id
---   --
---   local ender_chest_id.inventory = {}
--- end)
+
+-- runs when player is created
+script.on_event(defines.events.on_player_created, function(player)
+  -- gets player id
+  local player_id = player.player_index
+  -- creates unique id for player ender chest
+  local ender_chest_data = {
+    id = "ender_chest_" .. player_id,
+    inventory_data = {}
+  }
+end)
 
 -- runs when a gui is opened
 script.on_event(defines.events.on_gui_opened, function(event)
@@ -20,10 +21,12 @@ script.on_event(defines.events.on_gui_opened, function(event)
 
     -- adds player and ender chest as variables
     local player_id = event.player_index
-    local ender_chest_id = "ender_chest_" .. player_id
+    local ender_chest_data = {
+      id = "ender_chest_" .. player_id
+    }
 
     -- inserts ender chest inventory
-    ender_chest.insert(ender_chest_id.inventory)
+    ender_chest.insert(ender_chest_data.inventory_data)
   else
     return
   end
@@ -40,10 +43,12 @@ script.on_event(defines.events.on_gui_closed, function(event)
 
     -- adds player and ender chest as variables
     local player_id = event.player_index
-    local ender_chest_id = "ender_chest_" .. player_id
+    local ender_chest_data = {
+      id = "ender_chest_" .. player_id
+    }
 
     -- saves ender chest inventory
-    local ender_chest_id.inventory = get_inventory(ender_chest)
+    local ender_chest_data.inventory_data = get_inventory(ender_chest)
 
     -- clears contents of ender chest
     ender_chest.clear_items_inside()
